1850ae3f1eb08c7c43c34ca0383836605c355e52
Scott Griffis <scott@scottgriffis.com>
2017-04-18 04:28:31 +0000

    added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,11 @@
 class ProfilesController < ApplicationController
+  
+  #Devise authenticate_user! function to make sure users are logged in before allowing them to perform these functions
+  before_action :authenticate_user!
+  
+  #user can only edit their own info
+  before_action :only_current_user
+  
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
@@ -45,6 +52,12 @@ end
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      #current user is a Devise function
+      redirect_to(root_url) unless @user == current_user
     end
     
   
